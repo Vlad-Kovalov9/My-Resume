5efebc603d2768647c4c64e099f2716b7e2f23d7
Vlad <nowornever969@gmail.com>
2025-10-30 15:04:04 +0200

    Made Skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,3 +1,30 @@
+import s from "./Skills.module.css";
+import { skillsData } from "../../data/skills";
+
 export default function Skills() {
-  return;
+  return (
+    <div className={s.container}>
+      <div className={s.block}>
+        <h3 className={s.title}>Hard</h3>
+        <ul className={s.list}>
+          {skillsData.hard.map((skill, i) => (
+            <li key={i} className={s.item}>
+              {skill}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={s.block}>
+        <h3 className={s.title}>Soft</h3>
+        <ul className={s.list}>
+          {skillsData.soft.map((skill, i) => (
+            <li key={i} className={s.item}>
+              {skill}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
---
+++ src/data/skills.js
@@ -0,0 +1,29 @@
+export const skillsData = {
+  hard: [
+    "HTML5",
+    "CSS3",
+    "JavaScript (ES6+)",
+    "React",
+    "TailwindCSS",
+    "SCSS / SASS",
+    "Redux Toolkit",
+    "REST API",
+    "TypeScript",
+    "Node.js",
+    "MongoDB",
+    "Git / GitHub",
+    "Figma",
+    "Visual Studio Code",
+    "Vite",
+    "Responsive & Adaptive Design",
+  ],
+
+  soft: [
+    "Teamwork ",
+    "Communication",
+    "Responsibility",
+    "Problem-solving",
+    "Adaptability",
+    "Attention to Detail",
+  ],
+};
